47b9c7538b257d223b083c6b9038ff67bd4883a7
TuDang1 <dangquoctuhn@gmail.com>
2023-04-26 21:44:47 +0700

    Init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold text-gray-800">Hello, world!</h1>
+      <p className="text-gray-600 mt-2">
+        This is an example of a component using Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default App;
